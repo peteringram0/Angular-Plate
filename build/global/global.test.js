a58afa6b941eb1c6b81a90535df63738fe1ef04e
Peter Ingram <peter.ingram@hutchhouse.com>
2015-06-26 10:34:09 +0100

    added SCSS linting / setting up e2e tests

--- build/global/global.test.js
+++ build/global/global.test.js
@@ -1,12 +1,18 @@
-describe('test view1 module', function() {
+(function () {
 
-	beforeEach(module('app.factory'));
+	'use strict';
 
-	it('dataServiceFactory posts should be an array', inject(
-		function(dataServiceFactory) {
-			var posts = dataServiceFactory.getPosts();
-			expect({}.toString.call(posts)).toBe('[object Array]');
-		}
-	));
+	describe('test view1 module', function() {
 
-});
+		beforeEach(module('app.factory'));
+
+		it('dataServiceFactory posts should be an array', inject(
+			function(dataServiceFactory) {
+				var posts = dataServiceFactory.getPosts();
+				expect({}.toString.call(posts)).toBe('[object Array]');
+			}
+		));
+
+	});
+
+}());
